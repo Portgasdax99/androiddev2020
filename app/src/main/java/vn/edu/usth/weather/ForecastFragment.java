b4f3b817a680650bbf060ec1a8a3016898c5b981
Portgasdax99 <you@example.com>
2019-11-22 22:07:09 +0700

    fix color and using loop,random funtion for practical 4

--- app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
+++ app/src/main/java/vn/edu/usth/weather/ForecastFragment.java
@@ -11,20 +11,63 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.Random;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link ForecastFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link ForecastFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class ForecastFragment extends Fragment {
+    public ForecastFragment() {
+    }
+
+    private static final String days[] = {"Mon", "Tue", "Thus", "Wed", "Fri", "Sat", "Sun"};
+
+    //    private Integer[] imageArray = {
+//            R.drawable.bitcloudy,
+//            R.drawable.bitsnow,
+//            R.drawable.cloudy,
+//            R.drawable.cloudysunny,
+//            R.drawable.drizzle,
+//            R.drawable.heavyrain,
+//            R.drawable.partialsnow,
+//            R.drawable.rainy,
+//            R.drawable.snowy,
+//            R.drawable.storm,
+//            R.drawable.suncloud,
+//            R.drawable.sunny,
+//            R.drawable.thunder};
+//
+//    private final String weather[] = {
+//            "A bit cloudy",
+//            "A bit snow",
+//            "Cloudy",
+//            "Sunny and cloudy",
+//            "Drizzle",
+//            "Heavy rain",
+//            "Partial snow",
+//            "Rainy",
+//            "Shower snow",
+//            "Storm",
+//            "Sunny and a bit cloudy",
+//            "Sunny",
+//            "Thunder and lightning"
+//    };
     @Nullable
     @Override
-        public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_forecast,container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle savedInstanceState) {
+        ViewGroup v = (ViewGroup) inflater.inflate(R.layout.fragment_forecast, container, false);
+        LinearLayout rowContainer = v.findViewById(R.id.row_container);
+        Random r = new Random();
+
+        for (int i = 0; i < 14; i++) {
+            View row = inflater.inflate(R.layout.weather_row, null);
+            ((TextView) row.findViewById(R.id.weater_row_id)).setText(days[i % 7]);
+            String tempRange = (10 + r.nextInt() %10) +"-"+ (20 + r.nextInt() %10);
+            ((TextView) row.findViewById(R.id.temp)).setText(tempRange);
+            
+            rowContainer.addView(row);
+        }
+        return v;
     }
 }
